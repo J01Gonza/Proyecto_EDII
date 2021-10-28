652dcc5a21f181c49b03eb290799cf56f6ebd42b
J01Gonza <juanp01.gh@gmail.com>
2021-10-27 18:18:21 -0600

    Agregar archivos de proyecto.

---
+++ DLL/LZW.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public interface ILZW
+    {
+        public byte[] Compress(byte[] bytes);
+        public byte[] Decompress(byte[] bytes);
+    }
+    public class ImplementationClassL : ILZW
+    {
+        public byte[] Compress(byte[] bytes)
+        {
+            int byteQty;
+            int origDict;
+            List<int> comp = new List<int>();
+            List<byte> output = new List<byte>();
+            Dictionary<string, int> Legend = new Dictionary<string, int>();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (!Legend.ContainsKey(((char)bytes[i]).ToString()))
+                {
+                    Legend.Add(((char)bytes[i]).ToString(), Legend.Count + 1);
+                }
+            }
+            origDict = Legend.Count;
+            string w = "";
+            foreach (char k in bytes)
+            {
+                string wk = w + k;
+                if (Legend.ContainsKey(wk))
+                {
+                    w = wk;
+                }
+                else
+                {
+                    comp.Add(Legend[w]);
+                    Legend.Add(wk, Legend.Count + 1);
+                    w = k.ToString();
+                }
+            }
+            if (!string.IsNullOrEmpty(w))
+            {
+                comp.Add(Legend[w]);
+            }
+            byteQty = Convert.ToInt32((uint)Math.Log(Legend.Count, 2.0) + 1);
+            output.Add((byte)byteQty);
+            output.Add((byte)origDict);
+            int c = 0;
+            foreach (var item in Legend.Keys)
+            {
+                if (origDict > c)
+                {
+                    output.Add((byte)item[0]);
+                    c++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string codes = "";
+            foreach (var item in comp)
+            {
+                codes += DecBin(item, byteQty);
+            }
+            string bfin = "";
+            for (int i = 0; i < codes.Length; i++)
+            {
+                if (bfin.Length < 8)
+                {
+                    bfin += codes[i];
+                }
+                else
+                {
+                    long B = Convert.ToInt32(bfin);
+                    output.Add((byte)BinDec(B));
+                    bfin = codes[i].ToString();
+                }
+            }
+            if (!string.IsNullOrEmpty(bfin))
+            {
+                while (bfin.Length < 8)
+                {
+                    bfin += 0;
+                }
+                long B = Convert.ToInt32(bfin);
+                output.Add((byte)BinDec(B));
+            }
+            return output.ToArray();
+        }
+
+        public byte[] Decompress(byte[] bytes)
+        {
+            int bytesQty = Convert.ToInt32(bytes[0]);
+            int origDict = Convert.ToInt32(bytes[1]);
+            string codes = string.Empty;
+            string Out = string.Empty;
+            List<int> comp = new List<int>();
+            List<byte> output = new List<byte>();
+            Dictionary<int, string> Legend = new Dictionary<int, string>();
+            for (int i = 0; i < origDict; i++)
+            {
+                Legend.Add(Legend.Count + 1, ((char)bytes[2 + i]).ToString());
+            }
+            for (int i = 2 + origDict; i < bytes.Length; i++)
+            {
+                codes += DecBin(bytes[i], 8);
+            }
+            while (!string.IsNullOrEmpty(codes) && codes.Length >= bytesQty)
+            {
+                comp.Add(Convert.ToInt32(codes.Substring(0, bytesQty), 2));
+                codes = codes.Substring(bytesQty);
+            }
+            string w = Legend[comp[0]];
+            int p = comp[0];
+            comp.RemoveAt(0);
+            Out += w;
+            string k = "";
+            foreach (var item in comp)
+            {
+                if (item != 0)
+                {
+                    if (!Legend.ContainsKey(item))
+                    {
+                        k = Legend[p];
+                        k = k + w;
+                    }
+                    else
+                    {
+                        k = Legend[item];
+                    }
+                    Out += k;
+                    w = k.Substring(0, 1);
+                    Legend.Add(Legend.Count + 1, Legend[p] + w);
+                    p = item;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            foreach (var item in Out)
+            {
+                output.Add((byte)item);
+            }
+            return output.ToArray();
+        }
+
+        string DecBin(int decim, int m)
+        {
+            long bin = 0;
+            const int div = 2;
+            long digit = 0;
+            for (int i = decim % div, j = 0; decim > 0; decim /= div, i = decim % div, j++)
+            {
+                digit = i % div;
+                bin += digit * (long)Math.Pow(10, j);
+            }
+            string Bin = bin.ToString();
+            while (Bin.Length < m)
+            {
+                Bin = "0" + Bin;
+            }
+            return Bin;
+        }
+
+        int BinDec(long binary)
+        {
+            int decim = 0;
+            int digit = 0;
+            const int div = 10;
+
+            for (long i = binary, j = 0; i > 0; i /= div, j++)
+            {
+                digit = (int)i % div;
+                if (digit != 1 && digit != 0)
+                {
+                    return -1;
+                }
+                decim += digit * (int)Math.Pow(2, j);
+            }
+            return decim;
+        }
+
+    }
+}
---
+++ DLL/SDES.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public interface SDES
+    {
+        (string key1, string key2) generateKey(string mainKey, int[] P10, int[] P8);
+        byte Enconde(string mainKey, string key1, string key2, int[] P4, int[] EP, int[] IP, int[] IP1);
+    }
+
+    public class ImplementationClass : SDES
+    {
+        public (string key1, string key2) generateKey(string mainKey, int[] P10, int[] P8)
+        {
+            string key1 = "", key2 = "";
+            char[] key = mainKey.ToArray();
+            char[] pKey = new char[key.Length];
+            char[] left, right;
+            for (int i = 0; i < P10.Length; i++)
+            {
+                pKey[i] = key[P10[i] - 1];
+            }
+            left = leftShift(pKey.Take((pKey.Length + 1) / 2).ToArray());
+            right = leftShift(pKey.Skip((pKey.Length + 1) / 2).ToArray());
+            pKey = left.Concat(right).ToArray();
+            for (int i = 0; i < P8.Length; i++)
+            {
+                key1 += pKey[P8[i] - 1];
+            }
+            left = leftShift(leftShift(left));
+            right = leftShift(leftShift(right));
+            pKey = left.Concat(right).ToArray();
+            for (int i = 0; i < P8.Length; i++)
+            {
+                key2 += pKey[P8[i] - 1];
+            }
+            return (key1, key2);
+        }
+
+        public byte Enconde(string mainKey, string key1, string key2, int[] P4, int[] EP, int[] IP, int[] IP1)
+        {
+            string encode = "";
+            char[] key = mainKey.ToArray();
+            char[] k1 = key1.ToArray();
+            char[] k2 = key2.ToArray();
+            char[] pKey = new char[key.Length];
+            char[] p4 = new char[P4.Length];
+            char[] L, R, l, r, s;
+            string[,] S0 =
+{
+                { "01" , "00" , "11" , "10" },
+                { "11" , "10" , "01" , "00" },
+                { "00" , "10" , "01" , "11" },
+                { "11" , "01" , "11" , "10" }
+            };
+            string[,] S1 =
+            {
+                { "00" , "01" , "10" , "11" },
+                { "10" , "00" , "01" , "11" },
+                { "11" , "00" , "01" , "00" },
+                { "10" , "01" , "00" , "11" }
+            };
+            for (int i = 0; i < IP.Length; i++)
+            {
+                pKey[i] = key[IP[i] - 1];
+            }
+            L = pKey.Take((pKey.Length + 1) / 2).ToArray();
+            R = pKey.Skip((pKey.Length + 1) / 2).ToArray();
+            for (int i = 0; i < EP.Length; i++)
+            {
+                pKey[i] = R[EP[i] - 1];
+            }
+            for (int i = 0; i < k1.Length; i++)
+            {
+                if (pKey[i] == k1[i])
+                {
+                    pKey[i] = '0';
+                }
+                else
+                {
+                    pKey[i] = '1';
+                }
+            }
+            l = pKey.Take((pKey.Length + 1) / 2).ToArray();
+            r = pKey.Skip((pKey.Length + 1) / 2).ToArray();
+            s = (S0[Convert.ToInt32(l[0].ToString() + l[3].ToString(), 2), Convert.ToInt32(l[1].ToString() + l[2].ToString(), 2)] + S1[Convert.ToInt32(r[0].ToString() + r[3].ToString(), 2), Convert.ToInt32(r[1].ToString() + r[2].ToString(), 2)]).ToArray();
+            for (int i = 0; i < P4.Length; i++)
+            {
+                p4[i] = s[P4[i] - 1];
+            }
+            for (int i = 0; i < L.Length; i++)
+            {
+                if (p4[i] == L[i])
+                {
+                    p4[i] = '0';
+                }
+                else
+                {
+                    p4[i] = '1';
+                }
+            }
+            pKey = R.Concat(p4).ToArray();
+            L = pKey.Take((pKey.Length + 1) / 2).ToArray();
+            R = pKey.Skip((pKey.Length + 1) / 2).ToArray();
+            for (int i = 0; i < EP.Length; i++)
+            {
+                pKey[i] = R[EP[i] - 1];
+            }
+            for (int i = 0; i < k2.Length; i++)
+            {
+                if (pKey[i] == k2[i])
+                {
+                    pKey[i] = '0';
+                }
+                else
+                {
+                    pKey[i] = '1';
+                }
+            }
+            l = pKey.Take((pKey.Length + 1) / 2).ToArray();
+            r = pKey.Skip((pKey.Length + 1) / 2).ToArray();
+            s = (S0[Convert.ToInt32(l[0].ToString() + l[3].ToString(), 2), Convert.ToInt32(l[1].ToString() + l[2].ToString(), 2)] + S1[Convert.ToInt32(r[0].ToString() + r[3].ToString(), 2), Convert.ToInt32(r[1].ToString() + r[2].ToString(), 2)]).ToArray();
+            for (int i = 0; i < P4.Length; i++)
+            {
+                p4[i] = s[P4[i] - 1];
+            }
+            for (int i = 0; i < L.Length; i++)
+            {
+                if (p4[i] == L[i])
+                {
+                    p4[i] = '0';
+                }
+                else
+                {
+                    p4[i] = '1';
+                }
+            }
+            pKey = p4.Concat(R).ToArray();
+            for (int i = 0; i < IP1.Length; i++)
+            {
+                key[i] = pKey[IP1[i] - 1];
+            }
+            foreach (var item in key)
+            {
+                encode += item;
+            }
+            return Convert.ToByte(encode, 2);
+        }
+
+        static char[] leftShift(char[] array)
+        {
+            int size = array.Length;
+            char[] shift = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                shift[i] = array[(i + 1) % size];
+            }
+            return shift;
+        }
+    }
+}
