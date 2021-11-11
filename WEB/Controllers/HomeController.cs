787507ad594f0413b7a87de4c9fe82f3ac715699
Juan González <juanp01.gh@gmail.com>
2021-11-10 20:08:29 -0600

    Session Variables

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using WEB.Models;
 
 namespace WEB.Controllers
@@ -12,6 +10,7 @@ namespace WEB.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        const string SessionUser = "_User";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,6 +19,8 @@ namespace WEB.Controllers
 
         public IActionResult Index()
         {
+
+            ViewBag.Session = HttpContext.Session.GetString(SessionUser);
             return View();
         }
 
@@ -32,6 +33,46 @@ namespace WEB.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SignIn() { return View(); }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(IFormCollection collection)
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IActionResult SignUp() { return View(); }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignUp(IFormCollection collection)
+        {
+            try
+            {
+                var newUser = new Usuario
+                {
+                    Name = collection["Name"],
+                    LName = collection["LName"],
+                    User = collection["User"],
+                    Password = collection["Password"]
+                };
+                HttpContext.Session.SetString(SessionUser, newUser.User);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                return View();
+            }
         }
     }
 }
--- WEB/Models/Usuario.cs
+++ WEB/Models/Usuario.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WEB.Models
 {
     public class Usuario
     {
-        string user;
-        string password;
-        List<string> mensajes;
-        List<string> contactos;
-        int clave;
+        [Display (Name = "Usuario")]
+        [Required]
+        public string User { get; set; }
+        [Display(Name = "Nombre")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Apellido")]
+        [Required]
+        public string LName { get; set; }
+        [Display(Name = "Contraseña")]
+        [Required]
+        public string Password { get; set; }
+        public List<string> Messages { get; set; }
+        public List<string> Contacts { get; set; }
+        public int Key { get; set; }
     }
 }
