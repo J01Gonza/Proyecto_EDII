2dd115389e038bcd2eee7d6c1ecf585c328f3f5d
Juan González <juanp01.gh@gmail.com>
2021-11-18 19:25:43 -0600

    Password encryption

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Text;
+using DLL;
 
 namespace WEB.Controllers
 {
@@ -51,8 +52,10 @@ namespace WEB.Controllers
                 {
                     string json = ingreso.Content.ReadAsStringAsync().Result; 
                     User us = System.Text.Json.JsonSerializer.Deserialize<User>(json);
+                    var k = sdesKeys(us.key);
+                    string password = sdesEncode(collection["password"], k.key2, k.key1);
                     //DESCIFRAR CONTRASEÑA
-                    if (us.password == collection["password"])
+                    if (password == collection["password"])
                     {
                         //ingresa sesión
                     }
@@ -83,11 +86,12 @@ namespace WEB.Controllers
         {
             try
             {
+                var k = sdesKeys(collection["Name"].GetHashCode() % 256);
+                string password = sdesEncode(collection["password"], k.key1, k.key2);
                 var newUser = new User()
                 {
                     userName = collection["userName"],
-                    //CIFRAR CONTRASEÑA
-                    password = collection["Password"],
+                    password = password,
                     name = collection["Name"],
                     lName = collection["LName"],
                     contacts = new List<Contact>(),
@@ -114,6 +118,30 @@ namespace WEB.Controllers
                 return View();
             }
         }
-        
+
+        public (string key1, string key2) sdesKeys(int key)
+        {
+            int[] P10 = { 3, 5, 2, 7, 4, 10, 1, 9, 8, 6 };
+            int[] P8 = { 6, 3, 7, 4, 8, 5, 10, 9 };
+            sdes SDES = new sdes();
+            return SDES.generateKey(Convert.ToString(key, 2).PadLeft(10, '0'), P10, P8); ;
+        }
+
+        public string sdesEncode(string str, string key1, string key2)
+        {
+            List<byte> encode = new List<byte>();
+            int[] P4 = { 2, 4, 3, 1 };
+            int[] EP = { 4, 1, 2, 3, 2, 3, 4, 1 };
+            int[] IP = { 2, 6, 3, 1, 4, 8, 5, 7 };
+            int[] IP1 = { 4, 1, 3, 5, 7, 2, 8, 6 };
+            sdes SDES = new sdes();
+            byte[] bytes = Encoding.Unicode.GetBytes(str);
+            foreach (var item in bytes)
+            {
+                string mainKey = Convert.ToString(item, 2).PadLeft(8, '0');
+                encode.Add(SDES.Enconde(mainKey, key1, key2, P4, EP, IP, IP1));
+            }
+            return Encoding.Unicode.GetString(encode.ToArray());
+        }
     }
 }
